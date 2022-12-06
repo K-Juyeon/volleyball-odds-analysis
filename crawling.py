8d8364bc8c0b5f5b03c0bb43072394917819d561
JuyeonKim <85975158+K-Juyeon@users.noreply.github.com>
2022-12-07 00:53:49 +0900

    v 22.12.07.00.53

--- crawling.py
+++ crawling.py
@@ -4,11 +4,14 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+
+'''
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
 from selenium.webdriver.support.wait import WebDriverWait
+'''
 
 driver = selenium.webdriver.Chrome('./chromedriver/chromedriver')
 
