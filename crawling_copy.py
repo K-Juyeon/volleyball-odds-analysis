c3fb52918c88ff392417dcfddba4be1c2ab6e3e6
JuyeonKim <85975158+K-Juyeon@users.noreply.github.com>
2022-12-08 15:24:55 +0900

    v22.12.08.15.24

---
+++ crawling_copy.py
@@ -0,0 +1,9 @@
+import csv
+import time
+
+import selenium
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
